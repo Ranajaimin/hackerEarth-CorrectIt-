acfe825adc3f932a573491b19af83a4d2d4b6bea
Chintan <d.mallory2756@student.sbccd.edu>
2019-08-24 18:28:09 +0530

    just one problem time limit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 n = int(input())
 while n>0:
-    num = int(input())
-    while(num>0 and num!=1):
+    num = float(input())
+    while (num>0 and num!=1):
         if(num%2==0):
             num=num/2
         else:
